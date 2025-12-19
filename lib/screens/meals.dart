d2b2fdd56ed7f415bac0f33d3562b0a7f5144ef3
Selvababu93 <selvababu93@gmail.com>
2025-12-19 20:28:21 +0400

    meal item

---
+++ lib/screens/meals.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+import 'package:meals_app/models/meal.dart';
+import 'package:meals_app/widgets/meal_item.dart';
+
+class MealsScreen extends StatelessWidget {
+  const MealsScreen({super.key, required this.title, required this.meals});
+
+  final String title;
+  final List<Meal> meals;
+
+  @override
+  Widget build(context) {
+    Widget content = Center(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Text(
+            'Uh 0h ... No meals',
+            style: Theme.of(
+              context,
+            ).textTheme.headlineLarge!.copyWith(color: Theme.of(context).colorScheme.onSurface),
+          ),
+          const SizedBox(height: 16),
+          Text(
+            'Try selecting different category',
+            style: Theme.of(
+              context,
+            ).textTheme.bodyLarge!.copyWith(color: Theme.of(context).colorScheme.onSurface),
+          ),
+          Text('Test', style: TextStyle(color: Colors.white)),
+        ],
+      ),
+    );
+
+    if (meals.isNotEmpty) {
+      content = ListView.builder(
+        itemCount: meals.length,
+        itemBuilder: (ctx, index) => MealItem(meal: meals[index]),
+      );
+    }
+
+    return Scaffold(
+      appBar: AppBar(title: Text(title)),
+      body: content,
+    );
+  }
+}
---
+++ lib/widgets/category_grid_item.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:meals_app/models/category.dart';
+
+class CategoryGridItem extends StatelessWidget {
+  const CategoryGridItem({super.key, required this.category, required this.onSelectCategory});
+
+  final Category category;
+  final void Function() onSelectCategory;
+
+  @override
+  Widget build(context) {
+    return InkWell(
+      onTap: onSelectCategory,
+
+      // my approach
+      /*
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => MealsScreen(
+              title: category.title,
+              meals: dummyMeals.where((meal) => meal.categories.contains(category.id)).toList(),
+            ),
+          ),
+        );
+        */
+      splashColor: Theme.of(context).primaryColor,
+      borderRadius: BorderRadius.circular(16),
+      child: Container(
+        padding: const EdgeInsets.all(16),
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [category.color.withValues(alpha: 0.55), category.color.withValues(alpha: 0.9)],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+          ),
+          borderRadius: BorderRadius.circular(16),
+        ),
+        child: Text(
+          category.title,
+          style: Theme.of(
+            context,
+          ).textTheme.titleLarge!.copyWith(color: Theme.of(context).colorScheme.onSurface),
+        ),
+      ),
+    );
+  }
+}
